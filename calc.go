fb7bd19823bdb1746ebf7d8671260dbab5b4dc91
jun <junjie@keijet.com>
2021-06-11 16:56:05 +0800

    add main function

--- calc.go
+++ calc.go
@@ -1,5 +1,10 @@
-package calc
+package main
 
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
 // Add two numbers and return the result.
 func Add(val1, val2 int) int {
 	return val1 + val2
